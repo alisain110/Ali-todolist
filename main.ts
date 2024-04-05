42d5e1637fed0962b384e285b15318e2226646c5
alisain110 <164510224+alisain110@users.noreply.github.com>
2024-04-06 03:26:38 +0500

    todo list

---
+++ main.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer"
+
+let todo = []
+let condition = true;
+while (condition) {
+    
+
+let todos = await inquirer.prompt(
+    [
+        {
+            name :"todo",
+            type :"input",
+            message :"what would u like to add in our todos? "
+
+        },
+        {
+            name : "todolist",
+            type : "confirm",
+            message : "Are you sure ?",
+            default : "true"
+        },
+      
+    ]
+
+)
+todo.push(todos.todo);
+console.log(todo);
+condition = todos.todolist;
+
+
+}
+
